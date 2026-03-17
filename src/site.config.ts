d2dc8bd0dad0a47ba336d617003503b66d53e826
super-mortal <2169702639@qq.com>
2026-03-17 19:34:47 +0800

    初始化项目

---
+++ src/site.config.ts
@@ -0,0 +1,202 @@
+import type { CardListData, Config, IntegrationUserConfig, ThemeUserConfig } from 'astro-pure/types'
+
+export const theme: ThemeUserConfig = {
+  // [Basic]
+  /** Title for your website. Will be used in metadata and as browser tab title. */
+  title: 'super-mortal',
+  /** Will be used in index page & copyright declaration */
+  author: 'Mortal',
+  /** Description metadata for your website. Can be used in page metadata. */
+  description: 'super-mortal , 一个在数字世界中默默无闻的凡人',
+  /** The default favicon for your site which should be a path to an image in the `public/` directory. */
+  favicon: '/favicon/favicon.ico',
+  /** The default social card image for your site which should be a path to an image in the `public/` directory. */
+  socialCard: '/images/social-card.png',
+  /** Specify the default language for this site. */
+  locale: {
+    lang: 'zh-CN',
+    attrs: 'zh-CN',
+    // Date locale
+    dateLocale: 'zh-CN',
+    dateOptions: {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric'
+    }
+  },
+  /** Set a logo image to show in the homepage. */
+  logo: {
+    src: '/src/assets/touxiang.png',
+    alt: '头像'
+  },
+
+  titleDelimiter: '•',
+  prerender: true, // pagefind search is not supported with prerendering disabled
+  npmCDN: 'https://cdn.jsdelivr.net/npm',
+
+  // Still in test
+  head: [
+    {
+      tag: 'meta',
+      attrs: { name: 'keywords', content: '博客,技术,编程,前端,后端,开发' },
+      content: ''
+    }
+  ],
+  customCss: ['/src/assets/styles/toc.css'],
+
+  /** Configure the header of your site. */
+  header: {
+    menu: [
+      { title: '博客', link: '/blog' },
+      { title: '归档', link: '/archives' },
+      { title: '友链', link: '/links' },
+      { title: '标签', link: '/tags' },
+      { title: '关于', link: '/about' }
+    ]
+  },
+
+  /** Configure the footer of your site. */
+  footer: {
+    // Year format
+    year: `© 2026`,
+    links: [
+      // Registration link
+      {
+        title: '邮箱:2169702639@qq.com',
+        link: 'https://qm.qq.com/q/hWc7HZtKZW',
+        style: 'text-sm' // Uno/TW CSS class
+      },
+      // Privacy Policy link
+      {
+        title: 'Site Policy',
+        link: '/terms',
+        pos: 2 // position set to 2 will be appended to copyright line
+      }
+    ],
+    /** Enable displaying a “Astro & Pure theme powered” link in your site’s footer. */
+    credits: true,
+    /** Optional details about the social media accounts for this site. */
+    social: {} // 移除所有social图标，RSS已移到首页
+  },
+
+  // [Content]
+  content: {
+    /** External links configuration */
+    externalLinks: {
+      content: ' ↗',
+      /** Properties for the external links element */
+      properties: {
+        style: 'user-select:none'
+      }
+    },
+    /** Blog page size for pagination (optional) */
+    blogPageSize: 8,
+    // Currently support weibo, x, bluesky
+    share: []
+  }
+}
+
+export const integ: IntegrationUserConfig = {
+  // [Links]
+  // https://astro-pure.js.org/docs/integrations/links
+  links: {
+    // Friend logbook
+    logbook: [
+      { date: '2026-03-17', content: '添加morta友链' },
+      { date: '2025-03-16', content: '添加夏夜流萤友链' }
+    ],
+    // Yourself link info
+    applyTip: [
+      // 我的站点信息
+      { name: '站点名称', val: theme.title },
+      { name: '站点描述', val: theme.description || 'Null' },
+      { name: '站点链接', val: 'https://supermortal.cn' },
+      { name: '站点头像', val: 'https://q1.qlogo.cn/g?b=qq&nk=2169702639&s=640' }
+    ],
+    // Cache avatars in `public/avatars/` to improve user experience.
+    cacheAvatar: false
+  },
+  // [Search]
+  pagefind: true,
+  // Add a random quote to the footer (default on homepage footer)
+  // See: https://astro-pure.js.org/docs/integrations/advanced#web-content-render
+  // [Quote]
+  quote: {
+    // - Hitokoto
+    // https://developer.hitokoto.cn/sentence/#%E8%AF%B7%E6%B1%82%E5%9C%B0%E5%9D%80
+    // server: 'https://v1.hitokoto.cn/?c=i',
+    // target: `(data) => (data.hitokoto || 'Error')`
+    // - Quotable
+    // https://github.com/lukePeavey/quotable
+    // server: 'http://api.quotable.io/quotes/random?maxLength=60',
+    // target: `(data) => data[0].content || 'Error'`
+    // - DummyJSON
+    server: 'https://v1.hitokoto.cn/?c=i',
+  target: `(data) => (data.hitokoto || 'Error')`
+  },
+  // [Typography]
+  // https://unocss.dev/presets/typography
+  typography: {
+    class: 'prose text-base',
+    // The style of blockquote font `normal` / `italic` (default to italic in typography)
+    blockquoteStyle: 'italic',
+    // The style of inline code block `code` / `modern` (default to code in typography)
+    inlineCodeBlockStyle: 'modern'
+  },
+  // [Lightbox]
+  // A lightbox library that can add zoom effect
+  // https://astro-pure.js.org/docs/integrations/others#medium-zoom
+  mediumZoom: {
+    enable: true, // disable it will not load the whole library
+    selector: '.prose .zoomable',
+    options: {
+      className: 'zoomable'
+    }
+  },
+  // Comment system
+  waline: {
+    enable: false,
+    // Server service link
+    server: 'https://astro-theme-pure-waline.arthals.ink/',
+    // Show meta info for comments
+    showMeta: false,
+    // Refer https://waline.js.org/en/guide/features/emoji.html
+    emoji: ['bmoji', 'weibo'],
+    // Refer https://waline.js.org/en/reference/client/props.html
+    additionalConfigs: {
+      // search: false,
+      pageview: true,
+      comment: true,
+      locale: {
+        reaction0: 'Like',
+        placeholder: 'Welcome to comment. (Email to receive replies. Login is unnecessary)'
+      },
+      imageUploader: false
+    }
+  }
+}
+
+export const terms: CardListData = {
+  title: 'Terms content',
+  list: [
+    {
+      title: 'Privacy Policy',
+      link: '/terms/privacy-policy'
+    },
+    {
+      title: 'Terms and Conditions',
+      link: '/terms/terms-and-conditions'
+    },
+    {
+      title: 'Copyright',
+      link: '/terms/copyright'
+    },
+    {
+      title: 'Disclaimer',
+      link: '/terms/disclaimer'
+    }
+  ]
+}
+
+const config = { ...theme, integ } as Config
+export default config
