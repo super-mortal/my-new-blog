726e70b011d0fa687d98c00ee72713f8fb8e3263
super-mortal <2169702639@qq.com>
2026-04-09 19:08:44 +0800

    内容

--- src/site.config.ts
+++ src/site.config.ts
@@ -26,7 +26,7 @@ export const theme: ThemeUserConfig = {
   },
   /** Set a logo image to show in the homepage. */
   logo: {
-    src: '/src/assets/touxiang.png',
+    src: 'https://supermortal.cn/images/social-card.png',
     alt: '头像'
   },
 
